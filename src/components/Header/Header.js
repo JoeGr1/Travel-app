8cb8445ca297cb9847445e43ba78587d501df1d7
Yulia Trubacheva <yulitrub@gmail.com>
2023-03-17 03:13:59 +0000

    Add basic styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-
+import logo from "../../assets/icons/logo.svg";
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <>
+    <header>
       <nav className="navbar">
-        <Link to="/">
-          <img src="#" alt="Logo" className="navbar__logo" />
+        <Link to="/" className="navbar__link">
+          <img src={logo} alt="Logo" className="navbar__logo" />
+          <h3 className="navbar__title">WANDERLY</h3>
         </Link>
       </nav>
-    </>
+    </header>
   );
 };
 
--- src/pages/OccasionPage/OccasionPage.js
+++ src/pages/OccasionPage/OccasionPage.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "./OccasionPage.scss";
 
 const OccasionPage = () => {
   const { season, occasion } = useParams();
@@ -9,35 +10,35 @@ const OccasionPage = () => {
   return (
     <div className="occasion">
       <h1 className="occasion__title">{occasion}</h1>
-      <h2 className="occasion__subtitle">Choose your occasion</h2>
+      <h2 className="occasion__subtitle">Choose prefered continent</h2>
       <div className="occasion__card-wrapper">
         <Link to={`/${season}/${occasion}/europe`}>
-          <article className="occasion__ocasion-card">
-            <h3 className="occasion__card-title">Europe</h3>
+          <article className="occasion__continent-card occasion__continent-card--europe">
+            <h3 className="occasion__card-title">EUROPE</h3>
           </article>
         </Link>
         <Link to={`/${season}/${occasion}/south-america`}>
-          <article className="occasion__ocasion-card">
+          <article className="occasion__continent-card occasion__continent-card--south-america">
             <h3 className="occasion__card-title">SOUTH AMERICA</h3>
           </article>
         </Link>
         <Link to={`/${season}/${occasion}/north-america`}>
-          <article className="occasion__ocasion-card">
+          <article className="occasion__continent-card occasion__continent-card--north-america">
             <h3 className="occasion__card-title">NORTH AMERICA</h3>
           </article>
         </Link>
         <Link to={`/${season}/${occasion}/oceania`}>
-          <article className="occasion__ocasion-card">
+          <article className="occasion__continent-card occasion__continent-card--oceania">
             <h3 className="occasion__card-title">OCEANIA</h3>
           </article>
         </Link>
         <Link to={`/${season}/${occasion}/africa`}>
-          <article className="occasion__ocasion-card">
+          <article className="occasion__continent-card occasion__continent-card--africa">
             <h3 className="occasion__card-title">AFRICA</h3>
           </article>
         </Link>
         <Link to={`/${season}/${occasion}/asia`}>
-          <article className="occasion__ocasion-card">
+          <article className="occasion__continent-card occasion__continent-card--asia">
             <h3 className="occasion__card-title">ASIA</h3>
           </article>
         </Link>
